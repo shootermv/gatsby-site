a8c3de1748fbfd38084aa67af5f562b6ee6a5c8b
moshe vilner <shootermv@gmail.com>
2020-06-03 21:26:58 +0300

    post template should have header

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,9 +1,16 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+
 export default function Template({ data }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
+    <Layout>
+    <SEO title="Home" />
     <div className="blog-post">
       <h1>{frontmatter.title}</h1>
       <h2>{frontmatter.date}</h2>
@@ -12,6 +19,7 @@ export default function Template({ data }) {
         dangerouslySetInnerHTML={{ __html: html }}
       />
     </div>
+    </Layout>
   )
 }
 export const pageQuery = graphql`
