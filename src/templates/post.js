5086cf17d0a85adc5468f04c55d66f10493743fc
moshe vilner <shootermv@gmail.com>
2020-06-04 23:27:40 +0300

    ability to display images in posts

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 
